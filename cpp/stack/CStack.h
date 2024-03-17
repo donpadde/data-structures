ca3c0eb91a3585a6888ad48e5327228eca0ae765
donpadde <patrick.suing+github@mailbox.org>
2024-03-17 11:49:57 +0100

    adding c++ CStack, initial commit

---
+++ cpp/stack/CStack.h
@@ -0,0 +1,8 @@
+/**
+ * @file CStack.htmlinclude [block] file-name="cpp/stack/CStack.h"
+ * @brief Implementation of CStack class
+ * Detailed description follows here.
+ * @author Patrick Suing
+ * @date 2024-03-17
+ */
+
