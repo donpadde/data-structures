b54e2999816842ab4dccec5e57fd5ed96486782a
donpadde <patrick.suing+github@mailbox.org>
2024-04-03 19:26:53 +0200

    modified CStack.h

--- cpp/stack/CStack.h
+++ cpp/stack/CStack.h
@@ -6,3 +6,14 @@
  * @date 2024-03-17
  */
 
+class CStack {
+public:
+    CStack();
+    ~CStack();
+    void push(int value);
+    int pop();
+    int top();
+    bool isEmpty();
+    bool isFull();
+    void print();
+};
